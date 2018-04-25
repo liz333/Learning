81b72fdd5ba9841fbf29e01e7d147e4c91d2c432
Lizzy <lizzy.guanli@gmail.com>
2018-04-25 18:23:27 -0500

    Booleans!

--- 1/ViewController.swift
+++ 1/ViewController.swift
@@ -9,13 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    @IBOutlet weak var answer: UILabel!
+    
+    
+    @IBAction func botton3(_ sender: Any) {
+        print(text1)
+        print(text1.text!)
+        print(text2.text!)
+        
+        let addition:Bool = Double(text1.text!)! <= Double(text2.text!)!
+        if addition {answer.text = "It is \(Double(text1.text!)! + Double(text2.text!)!)"
+        } else {answer.text = "It is \(Double(text1.text!)! - Double(text2.text!)!)"}
+    }
+    
     @IBOutlet weak var Label: UILabel!
-  
+    
     var tapbuttoncount = 0
     
     @IBAction func buttontap(_ sender: Any) {
-    
+        
         tapbuttoncount = tapbuttoncount + 1
         if tapbuttoncount >= 20 {
             print("you tapped button" ,tapbuttoncount)
@@ -28,7 +46,7 @@ class ViewController: UIViewController {
     var tapbuttoncount2 = 0
     
     @IBAction func buttontap2(_ sender: Any) {
-    tapbuttoncount2 = tapbuttoncount2 + 1
+        tapbuttoncount2 = tapbuttoncount2 + 1
         if tapbuttoncount2 >= 10 {
             Label.text = "byebye"
         } else{
@@ -41,12 +59,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
